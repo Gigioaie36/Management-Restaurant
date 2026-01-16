32ca4d7471b69638d4b8454ff61b7dee1d9a4fb1
Grigore Cristian <constantin.cristian.grigore@gmail.com>
2026-01-16 10:25:12 +0200

    ce

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building and running RestaurantManager.Wpf...
-dotnet run --project RestaurantManager.Wpf
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo The application exited with an error (Exit Code: %ERRORLEVEL%).
-    pause
-)
